26d2b79afe94033062d73804bb99d22a75a0749a
abhishek-whistlemind <abhishek.lambate@whistlemind.com>
2026-05-19 09:46:09 +0530

    latest code

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,14 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
   return {
     plugins: [react()],
+    css: {
+      preprocessorOptions: {
+        scss: {
+          quietDeps: true,
+          silenceDeprecations: ['import', 'global-builtin', 'color-functions', 'if-function'],
+        },
+      },
+    },
     server: {
       proxy: {
         '/api': {
